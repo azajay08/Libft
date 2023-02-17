0db735953db9e0232c5ba489099a61b770e25908
azajay08 <aaron.p.j@hotmail.com>
2023-02-17 14:12:07 +0200

    added int line number for line struct

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: ajones <ajones@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 10:56:31 by ajones            #+#    #+#             */
-/*   Updated: 2023/01/18 14:51:50 by ajones           ###   ########.fr       */
+/*   Updated: 2023/02/17 14:11:46 by ajones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ typedef struct s_list
 
 typedef struct s_line
 {
+	int				num;
 	char			*line;
 	struct s_line	*next;
 }					t_line;
